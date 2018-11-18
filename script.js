5f81d7ef3ea6fe6c6883c2cf6a2a71ae185c94e3
jstudies <mdpmcm@gmail.com>
2018-11-19 00:17:07 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let myObj={first:"Dmitry",last:"Melnikov"};
+var shopping=['bread','milk','cheese']
+
+myObj.first="John";
+
+console.log(myObj);
+
+let myChair={color:"blue",wheels:4,material:"crap",age:"old"
+            }
+
+myObj['second']="second";
+let holder=1;
+myObj['a'+holder]="a123";
+console.log(myObj);
