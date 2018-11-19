3f79e13714115b718c3625489f93906e8bb913d7
jstudies <mdpmcm@gmail.com>
2018-11-19 11:22:08 +0200

    next

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let myArray=["Hello","World",50,false];
+console.log(myArray[0]+" "+myArray[1]);
+
+myArray[myArray.length+1]="New Item";
+myArray.push("At the end");
+let firstone=myArray.shift();
+console.log(firstone);
+
+myArray.unshift("New one at the start");
+
+myArray[5]="smth";
+delete myArray[3];
+
+let rep=myArray.splice(3,2,"One","Two")
+console.log(rep);
+
+
+
+
+
+/*let lastone=myArray.pop();
+console.log(lastone);  */
+
+/*
 let myObj={first:"Dmitry",last:"Melnikov"};
 var shopping=['bread','milk','cheese']
 
@@ -12,3 +36,4 @@ myObj['second']="second";
 let holder=1;
 myObj['a'+holder]="a123";
 console.log(myObj);
+*/
