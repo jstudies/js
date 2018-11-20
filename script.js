a5ab55060eab057147be915607a7864538130f17
jstudies <mdpmcm@gmail.com>
2018-11-20 17:23:46 +0200

    3-2

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
-var x=[5,77,344,"Hello","World"];
-var[a,b,c,d]=x;
-console.log(a,b,c,d);
-//console.log(z);
+let num=10;
+testOne(4);
+testOne("New");
+
+let a = testTwo(num);
+console.log(a);
+
+let b= "Hello";
+testThree(b);
+
+function testThree(str){
+    let b= "New";
+    console.log(b+" World");
+}
+
+function testTwo(arg){
+    return arg  * arg;
+}
+
+
+
+
+/*
+
+let num=0;
+
+testOne(4);
+testOne("New");
+let ret=testOne('WOW');
+console.log(ret);
+*/
+
+function testOne(val){
+    console.log(val);
+    let hello=val+" Hello World"
+    num++;
+    console.log(hello+" "+num);
+return hello;
+}
+
