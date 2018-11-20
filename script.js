1648191183a028cafae2830cab5d376a541d04cc
jstudies <mdpmcm@gmail.com>
2018-11-20 17:59:33 +0200

    block 3

--- script.js
+++ script.js
@@ -1,59 +1,35 @@
-let num=10;
-//testOne(4);
-//testOne("New");
-
-//let a = testTwo(num);
-//console.log(a);
-
-let b= "Hello";
-//testThree(b);
-console.log(testFour(5));
-console.log(testFour(5,8));
-
-console.log(test5(5));
-console.log(test5(5,8));
-
-function test5(x,y){
-//    y=(typeof y !=='undefined')?y:10;
-    y=y ||20;
-    return x*y;
+let messages = {
+    welcome: ['Welcome first', 'Welcome second'],
+    hello: function () {
+        console.log('Hello, how are you?')
+    },
+    goodbye: function () {
+        console.log('Thnks for coming Bye now')
+    },
+    output:function(mes){
+    console.log(mes)
 }
-
-
-function testFour(x,y){
-    console.log(x);
-    console.log(y);
-    return x*y;
-}
-
-
-function testThree(str){
-    let b= "New";
-//    console.log(b+" World");
-}
-
-function testTwo(arg){
-    return arg  * arg;
 }
 
 
 
 
-/*
 
-let num=0;
 
-testOne(4);
-testOne("New");
-let ret=testOne('WOW');
-console.log(ret);
-*/
 
-function testOne(val){
-//    console.log(val);
-    let hello=val+" Hello World"
-    num++;
-//    console.log(hello+" "+num);
-return hello;
+/*myFun5(3,5,6);
+
+function myFun5(a,b,c){
+    console.log(arguments.length);
+    console.log(arguments[1]);
 }
 
+let myFun3 = (x) => x * 10; // ES6
+
+let myFun1 = function (x) {
+    return x * 10;
+}
+
+function myFun2(x) {
+    return x * 10;
+}*/
