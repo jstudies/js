6eb8d1b2f2ebe1fbc569f57c1491ace8e28382cc
jstudies <mdpmcm@gmail.com>
2018-11-20 17:42:03 +0200

    3-4

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 let num=10;
-testOne(4);
-testOne("New");
+//testOne(4);
+//testOne("New");
 
-let a = testTwo(num);
-console.log(a);
+//let a = testTwo(num);
+//console.log(a);
 
 let b= "Hello";
-testThree(b);
+//testThree(b);
+console.log(testFour(5));
+console.log(testFour(5,8));
+
+console.log(test5(5));
+console.log(test5(5,8));
+
+function test5(x,y){
+//    y=(typeof y !=='undefined')?y:10;
+    y=y ||20;
+    return x*y;
+}
+
+
+function testFour(x,y){
+    console.log(x);
+    console.log(y);
+    return x*y;
+}
+
 
 function testThree(str){
     let b= "New";
-    console.log(b+" World");
+//    console.log(b+" World");
 }
 
 function testTwo(arg){
@@ -31,10 +50,10 @@ console.log(ret);
 */
 
 function testOne(val){
-    console.log(val);
+//    console.log(val);
     let hello=val+" Hello World"
     num++;
-    console.log(hello+" "+num);
+//    console.log(hello+" "+num);
 return hello;
 }
 
